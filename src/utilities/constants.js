494c116f70201195179ed8697bb6243183a510c4
SH <shahzaibsiddiqui101@gmail.com>
2024-07-18 15:26:14 +0500

    Cleanup for functionality

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -2,5 +2,5 @@ export const LOADING_STATUS = {
     LOADING: "loading", SUCCEEDED: "succeeded", FAILED: "failed", IDLE: "idle"
 }
 
-export const API_URL = "http://127.0.0.1:8000/api/";
-export const IMAGE_URL = "http://127.0.0.1:8000"
+export const API_Endpoint = "https://music.zetdigi.com/backend/public/api/";
+export const Asset_Endpoint = "https://music.zetdigi.com/backend/public/"
