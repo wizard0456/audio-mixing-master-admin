363122c0ea551b8a6c1c470eb3dc30a41f46ebe1
SH <shahzaibsiddiqui101@gmail.com>
2024-07-19 19:53:57 +0500

    Vite Config for server

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://audadmin.zetdigi.com/',
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: '/src',
+      },
+    ]
+  },
 })
