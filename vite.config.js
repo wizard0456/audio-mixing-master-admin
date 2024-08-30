99cf695e106eb8c3c31a1850c6d9023694e7243e
SH <shahzaibsiddiqui101@gmail.com>
2024-08-30 17:40:57 +0500

    Add Base URLs in config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'https://audadmin.zetdigi.com/',
+  // base: 'https://audiomixingmastering.com/admin',
+  // base: 'http://69.55.54.209/admin',
+  // base: 'https://69.55.54.209/admin',
   optimizeDeps: {
     include: ["@wojtekmaj/react-daterange-picker"]
   },
