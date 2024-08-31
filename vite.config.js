f82320a269b1632c39ade8e3bc69a84fd2e925da
SH <shahzaibsiddiqui101@gmail.com>
2024-08-31 20:18:40 +0500

    Add Live Domains

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'https://audadmin.zetdigi.com/',
+  // base: 'https://portal.zetdigitesting.online/',
   // base: 'https://audiomixingmastering.com/admin',
   // base: 'http://69.55.54.209/admin',
   // base: 'https://69.55.54.209/admin',
