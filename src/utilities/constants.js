e0ec77b6d6c86e7675d1064cb57139f427d59364
SH <shahzaibsiddiqui101@gmail.com>
2024-09-20 18:43:44 +0500

    add live domain and backend api

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,6 +1,6 @@
-export const API_Endpoint = "https://music.zetdigi.com/backend/public/api/";
-export const Asset_Endpoint = "https://music.zetdigi.com/backend/public/";
-// export const API_Endpoint = "https://backend.zetdigitesting.online/backend/public/api/";
-// export const Asset_Endpoint = "https://backend.zetdigitesting.online/backend/public/";
+// export const API_Endpoint = "https://music.zetdigi.com/backend/public/api/";
+// export const Asset_Endpoint = "https://music.zetdigi.com/backend/public/";
+export const API_Endpoint = "https://backend.zetdigitesting.online/api/";
+export const Asset_Endpoint = "https://backend.zetdigitesting.online/"
 
 export const Per_Page = 10;
