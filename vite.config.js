be5c9875d4f95e84d0bc8ce32677e9e046ea9b49
SH <shahzaibsiddiqui101@gmail.com>
2024-11-23 05:26:43 +0500

    Add Main Domain

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://audadmin.zetdigi.com/',
+  // base: 'https://audadmin.zetdigi.com/',
   // base: 'https://portal.zetdigitesting.online/',
-  // base: 'https://portal.audiomixingmastering.com/',
+  base: 'https://portal.audiomixingmastering.com/',
   optimizeDeps: {
     include: ["@wojtekmaj/react-daterange-picker"]
   },
